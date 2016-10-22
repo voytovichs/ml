4be717899352387a532bc49b6e9445375c565d47
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2016-10-22 18:51:28 +0300

    Remember - regression is broken

--- 02/regression.py
+++ 02/regression.py
@@ -10,18 +10,17 @@ class Regr(object):
         self._beta = None
         self._beta0 = None
 
-    def fit(self, X, y, mean_int=True):
+    def fit(self, X, y):
         self._fitted = True
-        self._beta = (y - y.mean()).dot((la.inv(X.dot(X.transpose())).dot(X)))
-        if mean_int:
-            self._beta0 = y.mean()
-        else:
-            self._beta0 = 1
+        ha = X.dot(X.T)
+        #self._beta = (y - y.mean()).dot((la.inv(X.dot(X.T)).dot(X)))
+        self._beta = la.inv(X * X.T)
+        self._beta0 = y.mean()
 
     def predict(self, X):
         if not self._fitted:
             raise RuntimeError('Fit regression before predicting')
-        y = self._beta * X.T + self._beta0
+        y = self._beta.T * X + self._beta0
         arr = [y[0, i] for i in range(y.shape[1])]
         return np.array(arr)
 
@@ -41,6 +40,21 @@ def split(X, y, seventy_five=False):
     return np.matrix(X_learn), np.matrix(X_test), np.array(y_learn), np.array(y_test)
 
 
+def split_array(X, y):
+    row = len(X)
+    for_test = [i % 2 == 0 for i in range(row)]
+    shuffle(for_test)
+    X_learn, X_test, y_learn, y_test = [], [], [], []
+    for i in range(row):
+        if for_test[i]:
+            X_test.append(X[i])
+            y_test.append(y[i])
+        else:
+            X_learn.append(X[i])
+            y_learn.append(y[i])
+    return np.array(X_learn), np.array(X_test), np.array(y_learn), np.array(y_test)
+
+
 def rmse(y, y_est):
     return np.sqrt(np.mean(((y - y_est) ** 2)))
 
@@ -52,7 +66,7 @@ def scale(X, mean, std):
         col = X[:, i]
         col_len = col.shape[0]
         scaled.append([(col[k] - mean[i]) / std[i] for k in range(col_len)])
-    return np.matrix(scaled).transpose()
+    return np.matrix(scaled).T
 
 
 def read_x(path, exclude_y=True):
@@ -90,13 +104,17 @@ def exclude_col(X, *args):
     return np.delete(X, args, axis=1)
 
 
-def cv(X, y, iterations=1000, log=False):
+def cv(X, y, array=False, iterations=1000, log=False):
     err = np.zeros(iterations)
-    X_l, X_t, y_l, y_t = split(X, y, seventy_five=False)
     for i in range(iterations):
+        X_l, X_t, y_l, y_t = split(X, y) if not array else split_array(X, y)
         r = Regr()
+        if array:
+            X_l = X_l.reshape(-1, 1)
+            X_t = X_t.reshape(-1, 1)
         r.fit(X_l, y_l)
-        y_est = r.predict(X_t)
+
+        y_est = r.predict(X_t if not array else np.matrix(X_t))
         err[i] = rmse(y_t, y_est)
     if log:
         print(err)
@@ -160,24 +178,56 @@ def backward_selection(X, y, min_col=1, cv_it=500):
     return to_delete
 
 
+def recover_index(a):
+    recovered = a[:]
+    for i in range(len(recovered)):
+        for k in range(i + 1, len(recovered)):
+            if recovered[k] > recovered[i]:
+                recovered[k] += 1
+    return recovered
+
+
+def exclude_col_except(X, *exc):
+    row, col = X.shape
+    to_delete = set(i for i in range(col)).difference(exc)
+    return exclude_col(X, *to_delete)
+
+
+def select_one_by_one(X, y, cv_iter, n):
+    selected = []
+    learn_set = exclude_col(X)  # weird kind of copying
+    for iter in range(n):
+        min_err = float('inf')
+        min_ind = -1
+        ind = min_ind
+        for col in learn_set.T:
+            ind += 1
+            cur_err = cv(col, y, array=True, iterations=cv_iter)
+            if cur_err < min_err:
+                min_err = cur_err
+                min_ind = ind
+        learn_set = exclude_col(learn_set, min_ind)
+        selected.append(min_ind)
+        print('Current error {}'.format(min_err))
+    return recover_index(selected)
+
 
 
 
 X = read_x('learn.csv')
 y = read_y('learn.csv')
 learn_mean, learn_std = get_mean_and_std(X)
-X_learn_scaled = scale(X, learn_mean, learn_std)
+X, y = get_rid_of_outliers(X, y, learn_mean, learn_std, m=10)
+selected = set(select_one_by_one(X, y, cv_iter=50, n=30))
+X = exclude_col_except(X, *selected)
 
-to_delete = backward_selection(X_learn_scaled, y, min_col=100, cv_it=10)
-X_learn_scaled_col = exclude_col(X_learn_scaled, *to_delete)
-X_learn_scaled_col_row, y_row = get_rid_of_outliers(X_learn_scaled_col, y, learn_mean, learn_std, m=25)
-
-X_test = read_x('test.csv', exclude_y=False)
-X_test_scaled = scale(X_test, learn_mean, learn_std)
-X_test_col_scaled = exclude_col(X_test_scaled, *to_delete)
+test = read_x('test.csv', exclude_y=False)
+test = exclude_col_except(test, *selected)
 
 r = Regr()
-r.fit(X_learn_scaled_col_row, y_row)
-y_test = r.predict(X_test_col_scaled)
-write('answer.csv', y_test)
-print(cv(X_learn_scaled_col_row, y_row, iterations=500))
+r.fit(X, y)
+#print(r._beta)
+
+answer = r.predict(test)
+print(answer)
+write('answer.csv', answer)
